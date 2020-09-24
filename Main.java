56ee77897d537ca4b0de1d0024e7efdbc890fb59
mohammad april <mapy.officialbawanna@gmail.com>
2020-09-24 02:51:21 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello Dunia :) ");
+    
+
+    //tested
+
+     System.out.println("jika : a = 10, b = 20, c=  30 ");
+    int a = 10;
+    int b = 20;
+    int c = 30;
+
+    System.out.println(" a + b + c = " +(a+b+c) );
+  }
+}
